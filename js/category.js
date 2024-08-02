bad06f29d470fd3f177b9db1a963120e5aa9230c
Pa-fs <qordi124@gmail.com>
2024-08-02 13:18:57 +0900

    modify my menu css

--- js/category.js
+++ js/category.js
@@ -3,18 +3,24 @@ import * as product from "./product.js";
 let categoryMenusList = document.querySelectorAll(".menu_category_list");
 
 function focusAfterClick(event) {
-  // 현재 선택된 메뉴에 'on' 클래스 제거
-  const currentActiveMenu = document.querySelector('.menu_category_list.active');
-  if (currentActiveMenu) {
-    currentActiveMenu.classList.remove('active');
+  // 메뉴 어느 곳이든 클릭했을 때 항상 같은 결과를 내도록 함
+
+  // 모든 메뉴 항목에서 on 클래스 제거
+  for (let i = 0; i < categoryMenusList.length; i++) {
+    categoryMenusList[i].querySelector("a").classList.remove("on");
   }
 
-  // 클릭한 메뉴에 'on' 클래스 추가
-  const targetMenu = event.target.closest('.menu_category_list');
-  targetMenu.classList.add('active');
+  // 이벤트 타겟에 따라 가장 가까운 해당 부모 요소 찾음
+  let targetMenu = event.target.closest(".menu_category_list");
 
-  // 데이터 렌더링
-  const categoryId = targetMenu.dataset.category;
+  // 부모 요소 찾고 자식 a태그에 on 클래스 추가
+  if (targetMenu) {
+    targetMenu.querySelector("a").classList.add("on");
+  }
+
+  // rendering
+  const categoryId = targetMenu.getAttribute("data-category");
+
   product.renderRecommendedProducts(categoryId);
   product.renderProducts(categoryId);
 }
