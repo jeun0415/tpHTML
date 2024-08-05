2b6cec3e5fb448dacc9a1e58f050629b67e4c09a
j_eun0415 <j_eun0415@naver.com>
2024-08-05 17:41:36 +0900

    address

--- js/ex03.js
+++ js/ex03.js
@@ -104,6 +104,43 @@ function renderPaymentMethod(id) {
                 - 결제하기 버튼을 클릭하여 계좌번호를 발급받고 해당 번호로 무통장 입금하시면 됩니다.</li>
         </ul>
       </div>
+
+      <div id="c-receipt">
+          <ul>
+              <li class="c-li6"><span style="font-size: 13pt;">현금영수증 신청</span>
+                  <div class="c-radio">
+                      <input class="c-prt1" type="radio" name="rr">&nbsp;발급&nbsp;&nbsp;&nbsp;
+                      <input class="c-prt2" type="radio" name="rr" checked>&nbsp;미발급
+                  </div>
+              </li>
+          </ul>
+          <div>
+              <ul class="c-type1">
+                  <input type="radio" name="aa" checked>&nbsp;소득공제용(일반 개인용)
+                  &nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">이름</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>&nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">휴대폰번호</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>
+              </ul>
+              <ul class="c-type2">
+                  <input type="radio" name="aa">&nbsp;지출증빙용(사업자용)
+                  &nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">사업자명</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>&nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">사업자번호</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>
+              </ul>
+          </div>
+      </div>
       `;
       break;
     case "3":
@@ -115,6 +152,42 @@ function renderPaymentMethod(id) {
                 - 계좌이체로 결제완료시 본인 계좌에서 즉시 이체 처리됩니다.</li><br>
             <li class="c-li">- 실시간 계좌이체는 은행별 이용시간이 다를 수 있습니다.</li>
         </ul>
+      </div>
+      <div id="c-receipt">
+          <ul>
+              <li class="c-li6"><span style="font-size: 13pt;">현금영수증 신청</span>
+                  <div class="c-radio">
+                      <input class="c-prt1" type="radio" name="rr">&nbsp;발급&nbsp;&nbsp;&nbsp;
+                      <input class="c-prt2" type="radio" name="rr" checked>&nbsp;미발급
+                  </div>
+              </li>
+          </ul>
+          <div>
+              <ul class="c-type1">
+                  <input type="radio" name="aa" checked>&nbsp;소득공제용(일반 개인용)
+                  &nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">이름</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>&nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">휴대폰번호</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>
+              </ul>
+              <ul class="c-type2">
+                  <input type="radio" name="aa">&nbsp;지출증빙용(사업자용)
+                  &nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">사업자명</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>&nbsp;&nbsp;&nbsp;
+                  <li class="c-li">
+                      <label for="">사업자번호</label>&nbsp;
+                      <input class="c-input" type="text">
+                  </li>
+              </ul>
+          </div>
       </div>
       `;
       break;
