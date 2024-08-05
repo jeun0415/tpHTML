3a9a3f530484aad783f90e55f2f17751dc99b9b3
j_eun0415 <j_eun0415@naver.com>
2024-08-05 13:16:44 +0900

    add address button

--- js/ex03.js
+++ js/ex03.js
@@ -131,3 +131,42 @@ function eraseProducts(parent) {
 }
 
 renderPaymentMethod("1");
+
+
+
+src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
+  const elZonecode = document.querySelector("#zonecode");
+  const elRoadAddress = document.querySelector("#roadAddress");
+  const elRoadAddressDetail = document.querySelector("#roadAddressDetail");
+  const elResults = document.querySelectorAll(".el_result");
+  // 주소검색창 열기 함수
+  const onClickSearch = () => {
+    console.log(1);
+    new daum.Postcode({
+      oncomplete: function (data) {
+        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+        // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+        console.log(data);
+        elZonecode.setAttribute("value", data.zonecode);
+        elRoadAddress.setAttribute("value", data.address);
+      },
+    }).open();
+  };
+  const register = () => {
+    console.log(`우편번호: ${elZonecode.getAttribute("value")}`);
+    console.log(`주소: ${elRoadAddress.getAttribute("value")}`);
+    console.log(`상세주소: ${elRoadAddressDetail.getAttribute("value")}`);
+    elResults[0].innerHTML = elZonecode.getAttribute("value");
+    elResults[1].innerHTML = elRoadAddress.getAttribute("value");
+    elResults[2].innerHTML = elRoadAddressDetail.getAttribute("value");
+  };
+  // 이벤트 추가
+  document.querySelector("#search-btn").addEventListener("click", () => {
+    onClickSearch();
+  });
+  document.querySelector("#register-btn").addEventListener("click", () => {
+    register();
+  });
+  elRoadAddressDetail.addEventListener("change", (e) => {
+    elRoadAddressDetail.setAttribute("value", e.target.value);
+  });
